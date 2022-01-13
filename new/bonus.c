c1d6b49adb9659eda61f84746de7722ea9a124e4
Ryojiro Akiyama <rakiyama@c1r30s8.42tokyo.jp>
2022-01-13 13:31:11 +0900

    add

--- new/bonus.c
+++ new/bonus.c
@@ -11,24 +11,16 @@ int	main(int argc, char *argv[])
 {
 	char *line;
 	char *file;
+	char *file2;
 	int fd;
+	int fd2;
 
-	if (argc == 1)//stdin
-	{
-		while (1)
-		{
-			line = get_next_line(STDIN_FILENO);
-			if (line)
-				printf("%s", line);
-			else
-				break;
-			free(line);
-		}
-	}
-	else if (argc == 2)//from file
+	if (argc == 3)
 	{
 		file = argv[1];
+		file2 = argv[2];
 		fd = open(file, O_RDONLY);
+		fd2 = open(file2, O_RDONLY);
 		while (1)
 		{
 			line = get_next_line(fd);
@@ -37,23 +29,15 @@ int	main(int argc, char *argv[])
 			else
 				break;
 			free(line);
-		}
-		close(fd);
-	}
-	else if (argc == 3)//error
-	{
-		while (1)
-		{
-			line = get_next_line(42);
+			line = get_next_line(fd2);
 			if (line)
 				printf("%s", line);
 			else
-			{
-				printf("ok");
 				break;
-			}
 			free(line);
 		}
+		close(fd);
+		close(fd2);
 	}
 	if (system("leaks a.out > /dev/null"))
 		system("leaks a.out");
